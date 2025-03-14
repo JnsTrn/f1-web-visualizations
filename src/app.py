bd614e21b4bcc4a18a645a2a4c90a32141001f8f
Jonas Truhn <stu240171@mail.uni-kiel.de>
2025-03-14 11:32:09 +0100

    changed app init

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
-app = Dash()
+app = Dash(__name__)
+server=app.server
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
